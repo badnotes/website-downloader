9ef518405d1b89f51d1af97b6de4a1c12593071a
jun.wan <whx10000@gmail.com>
2025-09-10 21:49:39 +0800

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sitedowner.com', // 设置网站URL
+  base: '/',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    build: {
+      minify: true, // 代码压缩
+    },
+    optimizeDeps: {
+      include: ['tailwindcss'],
+    },
+  },
+  // 配置全局页面元数据
+  integrations: [sitemap()],
+  // 配置Markdown选项
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true,
+    },
+  },
 });
